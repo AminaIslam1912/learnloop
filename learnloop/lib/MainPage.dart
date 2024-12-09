148c602738a9c34ba16a1e836065730bcd13e36e
AminaIslam1912 <jenny191277@gmail.com>
2024-12-09 19:45:07 +0600

    addint sent and received screen for  request option and adding landing page

---
+++ learnloop/lib/MainPage.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'request_sent.dart';
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [
+    Center(child: Text('Home Page')), // Home page placeholder
+    RequestPage(),                    // Requests page
+    Center(child: Text('Messages')),  // Messages page placeholder
+    Center(child: Text('Profile')),   // Profile page placeholder
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/background_app.png'), // Background image
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: _pages[_currentIndex],
+      ),
+      bottomNavigationBar: Container(
+        color: Color(0xFF264E70), // Background color for all icons
+        child: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          items: [
+            BottomNavigationBarItem(
+              icon: _buildIcon(Icons.home, isSelected: _currentIndex == 0),
+              label: "",
+            ),
+            BottomNavigationBarItem(
+              icon: _buildIcon(Icons.people, isSelected: _currentIndex == 1),
+              label: "",
+            ),
+            BottomNavigationBarItem(
+              icon: _buildIcon(Icons.message, isSelected: _currentIndex == 2),
+              label: "",
+            ),
+            BottomNavigationBarItem(
+              icon: _buildIcon(Icons.person, isSelected: _currentIndex == 3),
+              label: "",
+            ),
+          ],
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          backgroundColor: Color(0xFF264E70),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildIcon(IconData iconData, {required bool isSelected}) {
+    return Container(
+      decoration: BoxDecoration(
+        shape: BoxShape.rectangle,
+        color: isSelected ? Colors.white : Color(0xFF264E70), // Background color for selected icon
+      ),
+      padding: EdgeInsets.all(8),
+      child: Icon(
+        iconData,
+        color: isSelected ? Color(0xFF264E70) : Colors.white, // Icon color
+        size: 28,
+      ),
+    );
+  }
+}
---
+++ learnloop/lib/landing_page.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'MainPage.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // Navigate to the main page after 3 seconds
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => MainPage()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white, // Background color of the splash screen
+      body: Container(
+         decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/background_app.png'), // Background image
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/Writing practice.png', // Path to your splash screen image
+                height: 200, // Adjust the height as needed
+              ),
+              SizedBox(height: 20),
+              Text(
+                'LearnLoop',
+                style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFF264E70), // Customize text color
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
