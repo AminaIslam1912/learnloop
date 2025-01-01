e37de66442aed2f0b4fe974cc20edfb74ff117b4
AminaIslam1912 <jenny191277@gmail.com>
2025-01-02 01:34:32 +0600

    switching to real database

--- learnloop/lib/supabase_config.dart
+++ learnloop/lib/supabase_config.dart
@@ -1,8 +1,8 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class SupabaseConfig {
-  static const String supabaseUrl = 'https://fydubhxmhupqdoksymto.supabase.co';
-  static const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ5ZHViaHhtaHVwcWRva3N5bXRvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ5Mzc5MTcsImV4cCI6MjA1MDUxMzkxN30.YNpzZxqcNs6Odlc8YJOVnCnIkiQgacRYHBLd_OFuPoQ';
+  static const String supabaseUrl = 'https://fnzulmmhahivejgonepd.supabase.co';
+  static const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZuenVsbW1oYWhpdmVqZ29uZXBkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzUxODc2NDEsImV4cCI6MjA1MDc2MzY0MX0.IO9WN7cAVMDvzbXY_EWpOAvpLQcFDjqrWxpmxS3Mp8E';
 
   static SupabaseClient get client => Supabase.instance.client;
 
